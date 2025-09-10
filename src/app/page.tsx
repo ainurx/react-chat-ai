9429c18e464e43ef8c563bc9f5714b4b2d5f8af5
ainurx <ainurrahmat1@gmail.com>
2025-09-10 10:25:17 +0800

    [master] better chating UX

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,22 @@ const ai = new GoogleGenAI({
 export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [chat, setChat] = useState<Array<{isUser: boolean, message: string}>>([])
+  const [chatResponse, setChatresponse] = useState<string>("")
 
   const sendMessage = async (newChat: { isUser: boolean, message: string}) => {
+    if(chatResponse.length > 0) {
+      setChat((prevChat)=> [...prevChat, {isUser: false, message: chatResponse}])
+    }
     setChat((prevChat)=> [...prevChat, newChat])
+    setChatresponse("")
     setIsLoading(true)
-    const response = await ai.models.generateContent({
+    const response = await ai.models.generateContentStream({
       model: process.env.NEXT_PUBLIC_AI_MODEL as string,
       contents: newChat.message
     })
-    setChat((prevChat)=> [...prevChat, { isUser: false, message: response.text as string}])
+    for await (const chunk of response) {
+      setChatresponse((prev) => prev + ' ' + chunk.text)
+    }
     setIsLoading(false)
   }
 
@@ -35,17 +42,27 @@ export default function Home() {
             chat.map((c, index)=>( 
               <div key={index} className={`d-flex ${c.isUser ? 'justify-content-end' : ''} px-4 py-1`}>
                 <div style={c.isUser ? {backgroundColor: '#1C352D', padding: '8px', width: 'max-content', color: 'white', borderRadius: '8px', maxWidth: '600px'} : 
-                {backgroundColor: '#280A3E', padding: '8px', width: 'max-content', color: 'white', borderRadius: '8px', maxWidth: '600px'}}>
+                  {backgroundColor: '#280A3E', padding: '8px', width: 'max-content', color: 'white', borderRadius: '8px', maxWidth: '600px'}}>
                   {c.message}
                 </div>
               </div>
             ))
           }
           {
+            chatResponse && (
+              <div className={`d-flex px-4 py-1`}>
+                <div style={{backgroundColor: '#280A3E', padding: '8px', width: 'max-content', color: 'white', borderRadius: '8px', maxWidth: '600px'}}>
+                  {chatResponse}
+                </div>
+              </div>
+            )
+          }
+          {
             isLoading && 
             <div className='mx-4 py-4' style={{backgroundColor: '#280A3E', padding: '8px', color: 'white', borderRadius: '8px', maxWidth: '600px'}}>
-              <Placeholder animation='glow' striped variant='info' className='mb-2'>
-                <Placeholder md={12} />
+              <Placeholder animation='glow' variant='info' className='mb-2'>
+                <Placeholder md={12} className="rounded-pill" />
+                <Placeholder md={12} className="rounded-pill" />
               </Placeholder>
             </div>
           }
